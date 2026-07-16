823841e304256b5c64e57532bed3efb2e336b090
Franco Nogarin <spydmobile@gmail.com>
2026-07-16 11:56:09 -0600

    Fix Top of Page footer button: scroll to top instead of hijacking the hash route

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div id="footer">
       <ul>
-        <li id="left"><a href="#top">Top of Page</a></li>
+        <li id="left"><a href="#top" onClick={(e) => { e.preventDefault(); window.scrollTo(0, 0); }}>Top of Page</a></li>
         <li className="right" id="footer-date">Site Last Updated: 2026-07-15 3pm MDT</li>
       </ul>
     </div>
